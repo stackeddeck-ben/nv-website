768afbba7ea4418c14cce8e85965729657ffe324
korakot-ben <34016384+stackeddeck-ben@users.noreply.github.com>
2019-02-06 14:34:00 +0700

    nodejs and vuejs tutorial chepter 5

---
+++ server/src/app.js
@@ -0,0 +1,55 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+require('./routes')(app)
+
+app.get('/status', function(req, res){
+    res.send('Hello nodejs server')
+})
+
+app.get('/hello/:person', function (req,res){
+    console.log('hello - ' + req.params.person)
+    res.send('say hello with ' + req.params.person)
+})
+
+app.post('/hello', function(req, res){
+    res.send('OK you post - ' + req.body.name)
+})
+
+/* RESTFUL Api for users management */
+// create user
+app.post('/user', function (req,res){
+    res.send('สร้างผู้ใช้')
+})
+
+// edit user, suspend, active
+app.put('/user/:userId', function(req,res){
+    res.send('แก้ไขผู้ใช้')
+})
+
+// delete user
+app.delete('/user/:userId',function(req,res){
+    res.send('ลบผู้ใช้งาน')
+})
+
+// get user by id
+app.get('/user/:userId', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้งาน')
+})
+
+// get all user
+app.get('/users', function(req,res){
+    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+})
+
+
+let port = 8080
+
+app.listen(port, function(){
+    console.log('server running in ' + port)
+})
